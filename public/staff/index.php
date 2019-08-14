69ba5f646f97e86531f5b295402f28cf398930f1
Ahmad Mursaleen <ahmadmursaleen@gmail.com>
2019-08-14 15:34:30 +0200

    basic staff area templated with staff.css added

--- public/staff/index.php
+++ public/staff/index.php
@@ -4,11 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
+    <title>High School Admin</title>
 </head>
 <body>
-    <?php
-        echo "My first PHP script";
-    ?>
+    <header>
+    <h1>HS Staff Area</h1>
+    </header>
+    <nav>
+    <ul>
+    <li>
+    <a href="index.php">Menu</a>
+    </li>
+    </ul>
+    </nav>
+    <div id="content">
+    </div>
+    <footer>
+    &copy; <?php echo date('Y')?> High School
+    </footer>
 </body>
 </html>
