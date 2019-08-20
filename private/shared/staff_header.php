e4f8c8e31c33739180fdea316be6a053ddf71c5d
Ahmad Mursaleen <ahmadmursaleen@gmail.com>
2019-08-20 16:48:58 +0200

    paths added

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
-    <title>High School Admin</title>
+    <title>High School Admin <?php echo $page_title;?></title>
 </head>
 <body>
     <header>
--- public/staff/subjects/index.php
+++ public/staff/subjects/index.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        echo "My first PHP script";
-    ?>
-</body>
-</html>
+<?php
+require_once('../../../private/initialize.php');
+?>
+
+<?php  $page_title = 'Subjects Menu'; ?>
+
+
+<?php include('../../../private/shared/staff_header.php')?>
+
+<div id="content">
+</div>
+
+<?php include('../../../private/shared/staff_footer.php')?>
