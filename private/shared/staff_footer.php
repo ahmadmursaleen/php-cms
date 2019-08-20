71cff0e3baab90fbd46b5c0f4931c3a21630d387
Ahmad Mursaleen <ahmadmursaleen@gmail.com>
2019-08-20 16:24:03 +0200

    footer and header added

---
+++ private/shared/staff_footer.php
@@ -0,0 +1,5 @@
+<footer>
+    &copy; <?php echo date('Y')?> High School
+    </footer>
+</body>
+</html>
--- public/staff/index.php
+++ public/staff/index.php
@@ -1,27 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
-    <title>High School Admin</title>
-</head>
-<body>
-    <header>
-    <h1>HS Staff Area</h1>
-    </header>
-    <nav>
-    <ul>
-    <li>
-    <a href="index.php">Menu</a>
-    </li>
-    </ul>
-    </nav>
-    <div id="content">
-    </div>
-    <footer>
-    &copy; <?php echo date('Y')?> High School
-    </footer>
-</body>
-</html>
+<?php
+require_once('../../private/initialize.php');
+?>
+
+<?php include('../../private/shared/staff_header.php')?>
+
+<div id="content">
+</div>
+
+<?php include('../../private/shared/staff_footer.php')?>
