9f5bfe517eccc06dd734c291e3ccf0fb35a31a47
Ahmad Mursaleen <ahmadmursaleen@gmail.com>
2019-08-29 16:11:18 +0200

    url encoding implemented

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,4 +1,11 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
 <?php
 $id = $_GET['id'] ?? '1'; //PHP > 7.0
 echo $id;
 ?>
+
+
+<a href="show.php?name=<?php echo u('William Hague ');?>">Link</a><br/>
+<a href="show.php?name=<?php echo u('Buns&More ');?>">Link</a><br/>
+<a href="show.php?name=<?php echo u('$%&');?>">Link</a><br/>
