b681c121415f9c25a631c2be512c4a92ebc07e46
Ahmad Mursaleen <ahmadmursaleen@gmail.com>
2019-09-23 14:20:49 +0200

    form submission get vs post

--- public/staff/subjects/create.php
+++ public/staff/subjects/create.php
@@ -1,5 +1,9 @@
 <?php
-$menu_name = $_POST['menu_name'] ?? '';
+
+require_once('../../../private/initialize.php'); 
+
+if(is_post_request()){
+    $menu_name = $_POST['menu_name'] ?? '';
 $position = $_POST['position'] ?? '';
 $visible = $_POST['visible'] ?? '';
 
@@ -7,6 +11,10 @@ echo "Form Parameters <br/>";
 echo "menu_name".$menu_name." <br/>";
 echo "position".$position." <br/>";
 echo "visible".$visible." <br/>";
+}
 
+else{
+    redirect_to(url_for('/staff/subjects/new.php'));
+}
 
 ?>
